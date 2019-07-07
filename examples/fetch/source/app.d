b1d6ecadfea071703b001b1dcc3a48f7986d8eb6
Sebastiaan Koppe <mail@skoppe.eu>
2019-07-07 10:41:06 +0200

    update fetch example

--- examples/fetch/source/app.d
+++ examples/fetch/source/app.d
@@ -2,9 +2,13 @@ import spasm.bindings;
 import spasm.dom;
 import spasm.types;
 
+nothrow:
+@safe:
+
 extern (C) export void _start()
 {
-  auto promise = window.fetch(RequestInfo("https://reqres.in/api/users/2"));
+  auto requestInfo = RequestInfo("https://reqres.in/api/users/2");
+  auto promise = window.fetch(requestInfo);
   promise.then(r => r.json).then((data){
       console.log(data);
       console.log(data.as!(Json).data.avatar.as!string);
